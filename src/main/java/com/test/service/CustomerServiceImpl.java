ee7080be5946ed3ad4736423be62e86291c73fc9
laurentiumatei <laurentiumatei11@gmail.com>
2017-10-04 16:40:05 +0300

    Use ConfigurableBeanFactory
    ...instead of hardcoded bean scope
    value
    
    Change-Id: Ib3f31a7adb4d265b5fc06da7c5e47977f1e5030c

--- src/main/java/com/test/service/CustomerServiceImpl.java
+++ src/main/java/com/test/service/CustomerServiceImpl.java
@@ -3,6 +3,7 @@ package com.test.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
@@ -10,7 +11,7 @@ import com.test.model.Customer;
 import com.test.repository.CustomerRepository;
 
 @Service("customerService")
-@Scope("prototype")
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CustomerServiceImpl implements CustomerService {
 
 	@Autowired
